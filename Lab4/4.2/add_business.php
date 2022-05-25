311849a893f195d7f0c9f77e1c442d23cbdfa914
drake0401 <vuhoanglong72@gmail.com>
2022-05-25 22:05:10 +0700

    change lab 4.2

--- Lab4/4.2/add_business.php
+++ Lab4/4.2/add_business.php
@@ -7,15 +7,18 @@
     <title>Business</title>
     <script type="text/javascript">
 		function toggleClass(el) {
-
+            let str = el.id.toString();
+            el.style.backgroundColor = "black";
+            el.style.color = "white";
+            document.getElementById("category").value += str + " ";
 		}
 	</script>
 </head>
 <body>
     <div style="float: left; width: 250px">
-        <p>Click on one or control click on multiple catetories <p><br />
-        <div style="overflow: scroll; width:80%;">
-            <table border="1" width="100%" class="gridview">
+        <p>Click on one or control click on multiple catetories <p><br/>
+        <div>
+            <table border="1" style="overflow: scroll; width: 100%">
                 <?php 
                     $server = 'localhost';
                     $user = 'root';
@@ -28,8 +31,9 @@
                     $result = mysqli_query($connect, $query);
                     while($row = mysqli_fetch_array($result)){
                         $id = $row["CategoryID"];
-                        print '<tr onclick="toggleClass(this);" id="'.strval($id).'"
-                            <td>'.$id.'</td><br>
+                        $title = $row["Title"];
+                        print '<tr onclick="toggleClass(this);" id="'.strval($id).'">
+                            <td>'.$title.'</td>
                             </tr>';
                     }  
                     ?>                
@@ -45,7 +49,7 @@
             </tr>
             <tr>
                 <td>Address: </td>
-                <td><input type="text" name="adress"></td>
+                <td><input type="text" name="address"></td>
             </tr>
             <tr>
                 <td>City: </td>
@@ -61,10 +65,53 @@
             </tr>
             <tr>
                 <td><input type="submit" name="add_business" value="Add Business"></td>                
-                <td><input type="text" name="category" id="category"></td>
+                <td><input type="text" name="category" id="category" value="" style="display: none"></td>
             </tr>
         </table>	        
         </form>
     </div> 
+    <?php
+        if (array_key_exists("name", $_GET)){
+            $name = $_GET["name"];
+            $address = $_GET["address"];
+            $city = $_GET["city"];
+            $telephone = $_GET["telephone"];
+            $url = $_GET["url"];
+
+            $category_list = explode(" ",$_GET["category"]);
+            $cate_list = array();
+            for ($i = 0; $i < sizeof($category_list); $i++){
+                $tmp = strval($category_list[$i]);
+			    array_push($cate_list, $tmp);
+            }
+
+            $server = 'localhost';
+            $user = 'root';
+            $pass = '';
+            $mydb = 'lab4';
+
+            $connect = mysqli_connect($server, $user, $pass, $mydb);
+            if (!$connect){
+                die ("Failed connection!");
+            }
+            else{
+                $insertQuery = "INSERT INTO businesses VALUES (null, '$name','$address', '$city', '$telephone', '$url' )";
+                mysqli_query($connect, $insertQuery);
+                
+                $query = "SELECT * FROM businesses WHERE Name = '$name'";
+                $result = mysqli_query($connect, $query);
+                $row = mysqli_fetch_array($result);
+                $id = $row["BusinessId"];
+
+                for ($i = 0; $i < sizeof($cate_list); $i++ ){
+                    $cate = $cate_list[$i];
+                    $query = "INSERT INTO biz_category VALUES ($id, '$cate')";
+                    mysqli_query($connect, $query);
+                }
+                
+            }
+            mysqli_close($connect);
+        }
+    ?>
 </body>
 </html>
--- Lab4/4.2/list_business.php
+++ Lab4/4.2/list_business.php
@@ -5,8 +5,85 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List</title>
+    <script type="text/javascript">
+        function toggleClass(el){
+            var category = document.getElementsByClassName("category");
+            for (let i = 0; i < category.length; i++){
+                category[i].style.backgroundColor = "white";
+                category[i].style.color = "black";
+            }
+            el.style.backgroundColor = "black";
+            el.style.color = "white";
+
+            let id = el.id.toString();
+            let list_category = document.getElementsByClassName("listCategory");
+
+            for(let i=0; i<list_category.length; i++){
+    	        let category_member = list_category[i];
+    	        category_member.parentElement.style.display = 'none';
+            }
+
+		    for(let i=0; i<list_category.length; i++){
+		        let category_member = list_category[i];
+		        if(category_member.textContent.includes(id)){
+		        	category_member.parentElement.style.display = '';
+		        }
+		    }
+        }
+    </script>
 </head>
 <body>
-    
+    <div style="float: left; width: 450px">
+        <p>Click on one or control click on multiple catetories </p>
+        <div>
+            <table border="1" style="width: 60%">
+                <th>Click on a category to find businesses listing:</th>
+                <?php 
+                    $server = 'localhost';
+                    $user = 'root';
+                    $pass = '';
+                    $mydb = 'lab4';
+                    $table_name = 'categories';
+                    $connect = mysqli_connect($server, $user, $pass, $mydb);
+                    $query = "SELECT * FROM categories";
+                    
+                    $result = mysqli_query($connect, $query);
+                    while($row = mysqli_fetch_array($result)){
+                        $id = $row["CategoryID"];
+                        $title = $row["Title"];
+                        print '<tr onclick="toggleClass(this);" id="'.strval($id).'" class="category">
+                            <td>'.$title.'</td>
+                            </tr>';
+                    }  
+                    ?>                
+            </table>		
+        </div>
+    </div>  
+    <div>
+        <br><br><br>
+        <table border="1">
+            <?php 
+                $selectQuery = "SELECT * FROM businesses";
+                $result = mysqli_query($connect, $selectQuery);
+
+                while($row = mysqli_fetch_array($result)){
+                    print '<tr id="'.strval($row["BusinessId"]).'"><td>'.$row["BusinessId"].'</td>';
+                    print '<td>'.$row["Name"].'</td>';
+                    print '<td>'.$row["Address"].'</td>';
+                    print '<td>'.$row["City"].'</td>';
+                    print '<td>'.$row["Telephone"].'</td>';
+                    print '<td>'.$row["URL"].'</td>';
+                    print '<td style="display: none" class="listCategory">';
+                    
+                    $query1 = "SELECT * FROM biz_category WHERE BusinessId = ".$row['BusinessId'];
+                    $result1 = mysqli_query($connect, $query1);
+                    while ($row1 = mysqli_fetch_array($result1)){
+                        print $row1["CategoryId"];
+                    }
+                    print '</td></tr>';
+                }
+            ?>
+        </table>
+    </div> 
 </body>
 </html>
